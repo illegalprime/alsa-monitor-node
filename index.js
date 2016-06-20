de49e1a1f257c3241f9b6233538b9ff5f8279496
Michael Eden <themichaeleden@gmail.com>
2016-06-20 12:10:40 -0400

    allow removing and adding of listeners with events api

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-module.exports = require('bindings')('alsa.node');
+var EventEmitter = require("events").EventEmitter;
+var alsa_native = require("bindings")("alsa.node");
+
+var volume = new EventEmitter();
+
+alsa_native.monitor(function() {
+    volume.emit("change");
+});
+
+module.exports = {
+    volume: volume,
+};
